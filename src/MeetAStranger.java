82c12651b877c0653135cd693eea0cafe83d0ad8
Игорь Эглит <igeglits@gmail.com>
2023-11-21 15:00:23 +0200

    first

---
+++ src/MeetAStranger.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class MeetAStranger {
+    public static void main(String[] args) {
+        System.out.print("Hello, " + new Scanner(System.in).nextLine());
+
+    }
+}
