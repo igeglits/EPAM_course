b2f7d721b75f81857609f01f068b272bc9cb06c8
Игорь Эглит <igeglits@gmail.com>
2023-12-05 22:12:23 +0200

    arrays

--- src/ArrayMain.java
+++ src/ArrayMain.java
@@ -34,13 +34,15 @@ class ArrayMain {
         int[] arrayInt = {1, 2, 3, 4, 5, 6};
         for (int element : arrayInt) {
             element *= element;
-            System.out.println(element);
+            System.out.print(element + " ");
         }
+        System.out.println();
         for (int valueInt : arrayInt) {
-            System.out.println(valueInt);
+            System.out.print(valueInt + " ");
         }
         System.out.println("-------------------------------------------------------");
     }
+
 
     public static void MultiplicationTable() {
         int[][] multiplicationTable = new int[5][];
@@ -62,12 +64,13 @@ class ArrayMain {
         }
         System.out.println("------------------------------------------------");
     }
-    public static void MultiArraySum(){
-        int[][] array2D = { { 1, 2, 3, 4, 5 },
-                { 5, 4, 3, 2, 1 },
-                { 0, 2, 0, 4, 0 } };
+
+    public static void MultiArraySum() {
+        int[][] array2D = {{1, 2, 3, 4, 5},
+                {5, 4, 3, 2, 1},
+                {0, 2, 0, 4, 0}};
         int sum = 0;
-        for (int[]row : array2D) {
+        for (int[] row : array2D) {
             for (int element : row) {
                 sum = sum + element;
             }
@@ -80,11 +83,13 @@ class ArrayMain {
         }
         System.out.println("sum = " + sum);
         System.out.println("----------------------------------------------------");
-    }public static void RvanijMassiv(){
-        int[] numbers = { 1, 3, 5, 7, 9 };
+    }
+
+    public static void RvanijMassiv() {
+        int[] numbers = {1, 3, 5, 7, 9};
         int[][] array = new int[3][];
         array[0] = numbers;
-        array[2] = new int[] { 2, 4, 6, 8 };
+        array[2] = new int[]{2, 4, 6, 8};
         for (int[] row : array) {
             if (row != null) {
                 for (int element : row) {
@@ -95,9 +100,9 @@ class ArrayMain {
             }
             System.out.println();
         }
-        String[] ejgStr = new String[][] { { "null", "null" }, new String[] { "a", "b", "c" }, { new String() } }[0];
+        String[] ejgStr = new String[][]{{"null", "null"}, new String[]{"a", "b", "c"}, {new String()}}[0];
         String[] ejgStr1 = {"null"};
-        String[] ejgStr2 = { null };
+        String[] ejgStr2 = {null};
         System.out.println(ejgStr[0] + "  " + ejgStr2[0] + "  " + ejgStr1[0]);
         System.out.println("-------------------------------------------------------------------");
         /*String[] ejgStr = new String[][] { { null }, new String[] { "a", "b", "c" }, { new String() } }[0];
@@ -105,18 +110,19 @@ String[] ejgStr1 = null;
 String[] ejgStr2 = { null };
 System.out.println(ejgStr[0] + "  " + ejgStr2[0] + "  " + ejgStr1[0]); */
     }
-    public static void ArrayClassMethods(){
+
+    public static void ArrayClassMethods() {
         int[] array = {9, 8, 7, 6, 5};
         System.out.println(Arrays.toString(array));
         int[] newArray = Arrays.copyOf(array, 8);
         System.out.println(Arrays.toString(newArray));
-        int[] arr1 = {1,2,3,4,5,6,7,8,9};
-        int[] arr2 = {1,2,3,4,5,6,7,8,9};
-        int[] arr3 = {1,2,5,5,5,5,5,8,9};
+        int[] arr1 = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] arr2 = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] arr3 = {1, 2, 5, 5, 5, 5, 5, 8, 9};
         System.out.println(arr1 == arr2);//сравнение ссылок на разные объекты вернет "ложь", даже при равном количестве элементов и их идентичности.
         System.out.println(Arrays.equals(arr1, arr2));
         System.out.println(Arrays.equals(arr1, arr3));
-        int[] intArr = {55, 57, 61, 66, 18, 19, 27, 38,10, 55, 15, 39, 51, 18, 83, 95};
+        int[] intArr = {55, 57, 61, 66, 18, 19, 27, 38, 10, 55, 15, 39, 51, 18, 83, 95};
         Arrays.sort(intArr);
         System.out.println("The sorted int array is:");
         System.out.println(Arrays.toString(intArr));
